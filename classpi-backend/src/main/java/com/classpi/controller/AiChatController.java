c8890687b168efe585b5cee77d7d6ddb49f3cc69
TYHH <TYHH@example.com>
2026-06-28 10:50:29 +0800

    AI对话功能后端接口地址修改

--- classpi-backend/src/main/java/com/classpi/controller/AiChatController.java
+++ classpi-backend/src/main/java/com/classpi/controller/AiChatController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ai")
+@RequestMapping("/api/ai")
 @CrossOrigin
 public class AiChatController {
 
