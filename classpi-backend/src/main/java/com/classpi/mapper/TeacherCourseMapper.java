be6912d23f769d864a0fee5c298ca9678d6ec32c
向陈宇 <2944766645@qq.com>
2026-06-29 16:59:32 +0800

    Revert "刚刚漏的"
    This reverts commit 6be719b9032d11896df0bda20e791046cc58c57f.

--- classpi-backend/src/main/java/com/classpi/mapper/TeacherCourseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.classpi.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.classpi.entity.TeacherCourse;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TeacherCourseMapper extends BaseMapper<TeacherCourse> {
-}
