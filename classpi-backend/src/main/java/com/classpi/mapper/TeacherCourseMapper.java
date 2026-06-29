6be719b9032d11896df0bda20e791046cc58c57f
向陈宇 <2944766645@qq.com>
2026-06-29 15:49:14 +0800

    刚刚漏的

---
+++ classpi-backend/src/main/java/com/classpi/mapper/TeacherCourseMapper.java
@@ -0,0 +1,9 @@
+package com.classpi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.classpi.entity.TeacherCourse;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TeacherCourseMapper extends BaseMapper<TeacherCourse> {
+}
